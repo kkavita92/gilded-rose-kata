13127897a6b7a47c60d0aea8b899518c0144888b
Kavita <kkavita92@gmail.com>
2017-07-12 17:52:07 +0100

    Add tests for Backstage Passes

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -34,6 +34,53 @@ describe GildedRose do
   end
 
   describe 'backstage passes' do
+    describe 'sellin is more than 10 days away' do
+      it 'should increase quality by 1' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 12, 10)]
+        expect{ GildedRose.new(items).update_quality }.to change { items[0].quality }.by(1)
+      end
+
+      it 'should have maximum quality of 50' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 12, 50)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 50
+      end
+    end
+
+    describe 'sellin is 10 or less days away' do
+      it 'should increase quality by 2' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 10)]
+        expect{ GildedRose.new(items).update_quality }.to change { items[0].quality }.by(2)
+      end
+
+      it 'should have maximum quality of 50' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 50)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 50
+      end
+    end
+
+    describe 'sellin is 5 or less days away' do
+      it 'should increase quality by 3' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 10)]
+        expect{ GildedRose.new(items).update_quality }.to change { items[0].quality }.by(3)
+      end
+
+      it 'should have maximum quality of 50' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 50)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 50
+      end
+    end
+
+    describe 'sellin date has passed' do
+      it 'should have 0 quality' do
+        items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq 0
+      end
+    end
+
   end
 
 end
